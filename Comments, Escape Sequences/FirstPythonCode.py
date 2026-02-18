126eee3e28a589f4dde25c4d4046ed1e50e013d8
muzamil-dev-313 <karbalaimuzammil50@gmail.com>
2026-02-18 11:57:32 +0500

    Create FirstPythonCode.py

---
+++ Comments, Escape Sequences/FirstPythonCode.py
@@ -0,0 +1,11 @@
+# Comments, Escape Sequences & Print Statement 
+print("Hello World!")
+
+print("Hey I'm Muzamil a \"web developer\" \n and this is my first program in python. In this program I have used 'Escape Sequences character'.")  # \n  and \
+
+# We are going to code in python and I'm using a comment 
+print(7)
+print(7 + 9)
+
+print ("Hello Python", 7 , 6 , 5 , sep="|", end="4321 \n")
+print("Ohh! Amazing ")
