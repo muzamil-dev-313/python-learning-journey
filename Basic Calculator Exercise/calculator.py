5c494691828fdac5e32c9062077ca69de137fd0c
muzamil-dev-313 <karbalaimuzammil50@gmail.com>
2026-02-19 16:21:00 +0500

    Create calculator.py

---
+++ Basic Calculator Exercise/calculator.py
@@ -0,0 +1,11 @@
+# Creating a basic and easy calculator using two Variables 
+a = 89
+b = 76
+
+print("The sum of two varibles are =",a + b)
+print("The Subtraction of two varibles are =",a - b)
+print("The Multiplication of two varibles are =",a * b)
+print("The Modules of two varibles are =",a % b)
+print("The Floor Division of two varibles are =",a // b)
+print("The Division of two varibles are =",a / b)
+print("The exponential of two varibles are =",a ** b)
