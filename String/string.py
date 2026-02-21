80f946314324f8b2471390b273c1f13adc5f4e25
Muzamil <karbalaimuzammil50@gmail.com>
2026-02-21 13:04:28 +0500

    Create string.py

---
+++ String/string.py
@@ -0,0 +1,36 @@
+name = "Muzamil"
+is_student = "Yes"
+friend_name = "Ali Reza"
+AnotherFriend = 'Wajdan'
+
+print('My name is', name)
+
+string = "a string (str) is an \"immutable sequence of Unicode characters used for storing and manipulating text data."
+print(string)
+
+demo = 'a string (str) is an "immutable sequence" of Unicode characters used for storing and manipulating text data.'
+print(demo)
+
+poem = """Hope is the thing with feathers
+That perches in the soul,
+And sings the tune without the words
+And never stops at all.
+
+And sweetest in the gale is heard;
+And sore must be the storm
+That could abash the little bird
+That kept so many warm.
+
+I've heard it in the chilliest land
+And on the strangest sea;
+Yet, never, in extremity,
+It asked a crumb of me.
+
+—EMILY DICKINSON"""
+print(poem)
+
+print(name[0:5])
+print(name[:3])
+
+for test in poem:
+    print(test)
