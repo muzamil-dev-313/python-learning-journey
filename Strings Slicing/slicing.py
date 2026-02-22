a30935db5bafb08e8e3b0486dca3cc8fec8db917
Muzamil <karbalaimuzammil50@gmail.com>
2026-02-22 07:26:36 +0500

    Create slicing.py

---
+++ Strings Slicing/slicing.py
@@ -0,0 +1,35 @@
+# Print the length of the company string "Coding For All" using _len()_ method and _print()_.
+
+company = "Coding For All"
+
+print(len(company))
+print(company[0:8])
+
+# Exercise: Extract the Middle Three Characters from a String
+# Given a string, write a program to extract the middle three characters. 
+
+# Example:
+
+# Input: "JhonDipPeta"
+# Output: "Dip"
+# Input: "JaSonAy"
+# Output: "Son"
+# Alos find the length of the string
+name1 = "JhonDipPeta"
+name2 = "JaSonAy"
+
+print(name1[4:7])
+print(name2[2:5])
+print(len(name1))
+print(len(name2))
+
+MyName = "Muzamil"
+print(MyName[-5 : -2])
+print(MyName[-2 : -5])
+
+animal = "Lion"
+Which_animal = len(animal)
+print(Which_animal)
+print(animal[0:3])
+
+
