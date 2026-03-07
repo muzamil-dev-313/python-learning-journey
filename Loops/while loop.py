7e2fd10350ade0faa46bdbcdc031da197e83f5a0
Muzamil <karbalaimuzammil50@gmail.com>
2026-03-07 22:50:46 +0500

    Update while loop.py

--- Loops/while loop.py
+++ Loops/while loop.py
@@ -1,3 +1,8 @@
+user = ""
+while user != "Hello":
+    user = input("Enter your password: ")
+print("Welcome our website.")
+
 # Incrementing while loop
 i = 1
 while i < 10:
@@ -25,11 +30,67 @@ else:
     print("I am inside else")
 
 
-
 while True:
   number = int(input("Enter a positive number: "))
   print(number)
   if not number >= 0:
     break
 
+# Questionsl
 
+
+#-----------------------------------------------------------------------------
+# Print numbers from 1 to 10 using a while loop.
+num = 1
+while num <= 10:
+    print(num)
+    num += 1
+
+#-----------------------------------------------------------------------------
+# Print numbers from 10 to 1 (reverse order).
+num = 10
+while num > 0:
+    print(num)
+    num -= 1
+
+#-----------------------------------------------------------------------------
+# Print even numbers from 1 to 20 using a while loop.
+num = 1
+while num <= 20:
+    if num % 2 == 0:
+        print(num)
+    num += 1
+
+#-----------------------------------------------------------------------------
+# Print odd numbers from 1 to 15.
+num = 0
+while num <= 15:
+    if num % 2:
+        print(num)
+    num += 1
+
+#-----------------------------------------------------------------------------
+# Print the first 10 multiples of 5.
+num = 5
+count = 1
+
+while count <= 10:
+    print(num)
+    num += 5
+    count += 1
+
+# OR 
+
+num = 1
+while num <= 10:
+    print(num * 5)
+    num += 1
+#-----------------------------------------------------------------------------
+while True:
+    num = int(input("Enter a number: "))
+    if num == 10:
+        print(5 * num)
+        num += 1
+        break
+    else:
+        print("Invalid number try again.")
